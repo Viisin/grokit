07fa5d5d7f5a8160e84e0cbb7ed1696f34fa29dd
Ozpin.Lin <Ozpin.Lin@infortrend.com>
2017-03-29 14:49:49 +0800

    grokit: update tools, add gitignore for generated files

--- src/platdefines.py
+++ src/platdefines.py
@@ -8,91 +8,93 @@ changing this file should be good enough
 import platform
 
 if 'Windows' in platform.platform():
-    import ntpath
+	import ntpath
 
-    def convert_to_os_path(path):
-        newpath = ntpath.normpath(path)
-        return newpath
-    if platform.architecture()[0] == '32bit':
-        tools_dict = \
-        { 'tomcat' : { 'file_name': 'apache-tomcat-8.0.8.tar.gz',
-                       'file_size': 9098084,
-                       'extract_path': 'script_path',
-                       'extract_dir' : 'apache-tomcat-8.0.8',
-                       'bin_file': '.bat'
-                      },
-          'opengrok' : { 'file_name' : 'opengrok-0.12.1.tar.gz',
-                         'file_size': 14506193,
-                         'extract_path': 'script_path',
-                         'extract_dir' : 'opengrok-0.12.1',
-                         'bin_file': 'opengrok.jar'
-                        },
-          'jre': { 'file_name' : 'jre-7u65-windows-i586.gz',
-                   'file_size': 41912320,
-                   'extract_path': 'script_path',
-                   'extract_dir' : 'jre1.7.0_65',
-                   'bin_file': 'java.exe'
-                  },
-          'ctags': { 'file_name' : 'exuberant-ctags.zip',
-                     'file_size': 571849,
-                     'extract_path': 'script_path',
-                     'extract_dir' : 'ctags58',
-                     'bin_file': 'ctags.exe'
-                    }
-        }
-    elif platform.architecture()[0] == '64bit':
-        tools_dict = \
-        { 'tomcat' : { 'file_name': 'apache-tomcat-8.0.8.tar.gz',
-                       'file_size': 9098084,
-                       'extract_path': 'script_path',
-                       'extract_dir' : 'apache-tomcat-8.0.8',
-                       'bin_file': '.bat'
-                      },
-          'opengrok' : { 'file_name' : 'opengrok-0.12.1.tar.gz',
-                         'file_size': 14506193,
-                         'extract_path': 'script_path',
-                         'extract_dir' : 'opengrok-0.12.1',
-                         'bin_file': 'opengrok.jar'
-                        },
-          'jre': { 'file_name' : 'jre-7u65-windows-x64.gz',
-                   'file_size': 43704320,
-                   'extract_path': 'script_path',
-                   'extract_dir' : 'jre1.7.0_65',
-                   'bin_file': 'java.exe'
-                  },
-          'ctags': { 'file_name' : 'exuberant-ctags.zip',
-                     'file_size': 571849,
-                     'extract_path': 'script_path',
-                     'extract_dir' : 'ctags58',
-                     'bin_file': 'ctags.exe'
-                    }
-        }
+	def convert_to_os_path(path):
+		newpath = ntpath.normpath(path)
+		return newpath
+	if platform.architecture()[0] == '32bit':
+		tools_dict = \
+		{ 'tomcat' : { 'file_name': 'apache-tomcat-9.0.0.M15.tar.gz',
+					   'file_size': 10418920,
+					   'extract_path': 'script_path',
+					   'extract_dir' : 'apache-tomcat-9.0.0.M15',
+					   'bin_file': '.bat'
+					  },
+		'opengrok' : { 'file_name' : 'opengrok-0.13-rc5.tar.gz',
+					   'file_size': 16113249,
+					   'extract_path': 'script_path',
+					   'extract_dir' : 'opengrok-0.13-rc5',
+					   'bin_file': 'opengrok.jar'
+					  },
+		'jre': { 'file_name' : 'jre-8u121-windows-i586.tar.gz',
+				 'file_size': 62043176,
+				 'extract_path': 'script_path',
+				 'extract_dir' : 'jre1.8.0_121',
+				 'bin_file': 'java.exe'
+				},
+		'ctags': { 'file_name' : 'exuberant-ctags.zip',
+				   'file_size': 571849,
+				   'extract_path': 'script_path',
+				   'extract_dir' : 'ctags58',
+				   'bin_file': 'ctags.exe'
+				  }
+		}
+	elif platform.architecture()[0] == '64bit':
+		tools_dict = \
+		{ 'tomcat' : { 'file_name': 'apache-tomcat-9.0.0.M15.tar.gz',
+					   'file_size': 10418920,
+					   'extract_path': 'script_path',
+					   'extract_dir' : 'apache-tomcat-9.0.0.M15',
+					   'bin_file': '.bat'
+					  },
+		'opengrok' : { 'file_name' : 'opengrok-0.13-rc5.tar.gz',
+					   'file_size': 16113249,
+					   'extract_path': 'script_path',
+					   'extract_dir' : 'opengrok-0.13-rc5',
+					   'bin_file': 'opengrok.jar'
+					  },
+		'jre': { 'file_name' : 'jre-8u121-windows-x64.tar.gz',
+				 'file_size': 65704280,
+				 'extract_path': 'script_path',
+				 'extract_dir' : 'jre1.8.0_121',
+				 'bin_file': 'java.exe'
+				},
+		'ctags': { 'file_name' : 'exuberant-ctags.zip',
+				   'file_size': 571849,
+				   'extract_path': 'script_path',
+				   'extract_dir' : 'ctags58',
+				   'bin_file': 'ctags.exe'
+				  }
+		}
+
 elif 'Linux' in platform.platform():
-    import posixpath
-    def convert_to_os_path(path):
-        newpath = posixpath.normpath(path)
-        return newpath
+	import posixpath
+	def convert_to_os_path(path):
+		newpath = posixpath.normpath(path)
+		return newpath
 
-    tools_dict = \
-    { 'tomcat' : { 'file_name': 'apache-tomcat-8.0.8.tar.gz',
-                   'file_size': 9098084,
-                   'extract_dir' : 'apache-tomcat-8.0.8',
-                   'bin_file': '.sh'
-                  },
-      'opengrok' : { 'file_name' : 'opengrok-0.12.1.tar.gz',
-                     'file_size': 14506193,
-                     'extract_dir' : 'opengrok-0.12.1',
-                     'bin_file': 'opengrok.jar'
-                    },
-      'jre': { 'file_name' : 'jre-7u65-linux-x64.tar.gz',
-               'file_size': 47005094,
-               'extract_dir' : 'jre1.7.0_65',
-               'bin_file': 'java'
-              },
-      'ctags': { 'file_name' : 'exuberant-ctags.tar',
-                 'file_size': 921600,
-                 'extract_dir' : 'ctags58',
-                 'bin_file': 'ctags'
-                }
-    }
+	tools_dict = \
+	{ 'tomcat' : { 'file_name': 'apache-tomcat-8.0.8.tar.gz',
+				   'file_size': 9098084,
+				   'extract_dir' : 'apache-tomcat-8.0.8',
+				   'bin_file': '.sh'
+				  },
+	  'opengrok' : { 'file_name' : 'opengrok-0.13-rc5.tar.gz',
+					 'file_size': 16113249,
+					 'extract_path': 'script_path',
+					 'extract_dir' : 'opengrok-0.13-rc5',
+					 'bin_file': 'opengrok.jar'
+					},
+	  'jre': { 'file_name' : 'jre-8u121-linux-x64.tar.gz',
+			   'file_size': 73676107,
+			   'extract_dir' : 'jre1.7.0_65',
+			   'bin_file': 'java'
+			  },
+	  'ctags': { 'file_name' : 'exuberant-ctags.tar',
+				 'file_size': 921600,
+				 'extract_dir' : 'ctags58',
+				 'bin_file': 'ctags'
+				}
+	}
 
